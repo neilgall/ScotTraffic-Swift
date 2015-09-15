436eac1bc609357ca3b3ebe4e22006f03215caa9
Neil Gall <neilgall@mac.com>
2015-09-15 21:47:06 +0100

    Initial Commit

---
+++ ScotTraffic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScotTraffic
+//
+//  Created by Neil Gall on 15/09/2015.
+//  Copyright © 2015 Neil Gall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
