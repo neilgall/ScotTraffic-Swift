8b09d517cf5a637d8120e4a4e67f3c9b7703e563
Neil Gall <neilgall@mac.com>
2016-08-17 21:53:42 +0100

    Resizable traffic cameras extension working. Finally!

--- TrafficCamerasExtension/TodayViewController.swift
+++ TrafficCamerasExtension/TodayViewController.swift
@@ -18,23 +18,25 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet var errorLabel: UILabel!
     @IBOutlet var leftButton: UIButton!
     @IBOutlet var rightButton: UIButton!
-    @IBOutlet var infoContainer: UIView!
-    @IBOutlet var imageContainer: UIView!
-    @IBOutlet var weatherContainer: UIView!
+    @IBOutlet var mainStackView: UIStackView!
+    @IBOutlet var infoStackView: UIStackView!
     
     let model = TodayViewModel()
     var receivers: [ReceiverType] = []
-
-    let widgetLayoutGuide = UILayoutGuide()
-    var widgetHeightConstraint: NSLayoutConstraint!
-    var widgetWidthConstraint: NSLayoutConstraint!
-    var expandedDisplayModeConstraints: [NSLayoutConstraint] = []
-    var compactDisplayModeConstraints: [NSLayoutConstraint] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOSApplicationExtension 10.0, *) {
+            extensionContext?.widgetLargestAvailableDisplayMode = .Expanded
+            titleLabel.textColor = .blackColor()
+            temperatureLabel.textColor = .blackColor()
+        }
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
 
-        ensureConstraintsCreated()
         errorLabel?.hidden = true
         
         receivers.append(model.image --> {
@@ -82,14 +84,11 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     @available(iOSApplicationExtension 10.0, *)
     func widgetActiveDisplayModeDidChange(activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
-        ensureConstraintsCreated()
-        widgetWidthConstraint.constant = maxSize.width
-        widgetHeightConstraint.constant = maxSize.height
         switch activeDisplayMode {
         case .Expanded:
-            transitionFromConstraints(compactDisplayModeConstraints, toConstraints: expandedDisplayModeConstraints)
+            layoutForExpandedDisplayMode(maxSize: maxSize)
         case .Compact:
-            transitionFromConstraints(expandedDisplayModeConstraints, toConstraints: compactDisplayModeConstraints)
+            layoutForCompactDisplayMode(maxSize: maxSize)
         }
     }
     
@@ -101,149 +100,15 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         model.moveToNextImage()
     }
 
-    private func ensureConstraintsCreated() {
-        guard widgetWidthConstraint == nil
-            else { return }
-        
-        [infoContainer, imageContainer, imageView, titleLabel, leftButton, rightButton, errorLabel, weatherIconImageView, temperatureLabel].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        view.addLayoutGuide(widgetLayoutGuide)
-        
-        NSLayoutConstraint.activateConstraints([
-            // layout guide centred on view
-            widgetLayoutGuide.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor),
-            widgetLayoutGuide.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor),
-            
-            // Image view fills container with 4:3 aspect
-            imageView.centerXAnchor.constraintEqualToAnchor(imageContainer.centerXAnchor),
-            imageView.centerYAnchor.constraintEqualToAnchor(imageContainer.centerYAnchor),
-            imageView.widthAnchor.constraintEqualToAnchor(imageContainer.widthAnchor),
-            imageView.heightAnchor.constraintEqualToAnchor(imageContainer.heightAnchor),
-            imageView.heightAnchor.constraintEqualToAnchor(imageView.widthAnchor, multiplier: 0.75),
-            
-            // Error label centred on image view
-            errorLabel.centerXAnchor.constraintEqualToAnchor(imageContainer.centerXAnchor),
-            errorLabel.centerYAnchor.constraintEqualToAnchor(imageContainer.centerYAnchor),
-            errorLabel.widthAnchor.constraintLessThanOrEqualToAnchor(imageContainer.widthAnchor),
-            
-            // Left/right buttons cover image view equally
-            leftButton.leadingAnchor.constraintEqualToAnchor(imageContainer.leadingAnchor),
-            rightButton.leadingAnchor.constraintEqualToAnchor(leftButton.trailingAnchor),
-            rightButton.trailingAnchor.constraintEqualToAnchor(imageContainer.trailingAnchor),
-            leftButton.centerYAnchor.constraintEqualToAnchor(imageContainer.centerYAnchor),
-            leftButton.centerYAnchor.constraintEqualToAnchor(imageContainer.centerYAnchor),
-            rightButton.heightAnchor.constraintEqualToAnchor(imageContainer.heightAnchor),
-            rightButton.heightAnchor.constraintEqualToAnchor(imageContainer.heightAnchor),
-            leftButton.widthAnchor.constraintEqualToAnchor(rightButton.widthAnchor),
-            
-            // title label is at top of info
-            titleLabel.topAnchor.constraintEqualToAnchor(infoContainer.topAnchor) ~ "title at top",
-
-            // weather inside weather container
-            temperatureLabel.topAnchor.constraintEqualToAnchor(weatherContainer.topAnchor),
-            temperatureLabel.bottomAnchor.constraintEqualToAnchor(weatherContainer.bottomAnchor),
-            weatherIconImageView.topAnchor.constraintEqualToAnchor(weatherContainer.topAnchor),
-            weatherIconImageView.bottomAnchor.constraintEqualToAnchor(weatherContainer.bottomAnchor),
-            temperatureLabel.leadingAnchor.constraintEqualToAnchor(weatherContainer.leadingAnchor),
-            temperatureLabel.trailingAnchor.constraintEqualToAnchor(weatherIconImageView.trailingAnchor),
-            weatherIconImageView.trailingAnchor.constraintEqualToAnchor(weatherContainer.trailingAnchor),
-
-            // weather container is at bottom of info
-            weatherContainer.bottomAnchor.constraintEqualToAnchor(infoContainer.bottomAnchor),
-            
-            // weather icon is square
-            weatherIconImageView.heightAnchor.constraintLessThanOrEqualToConstant(44) ~ (UILayoutPriorityRequired-1) ~ "weather icon height",
-            weatherIconImageView.widthAnchor.constraintEqualToAnchor(weatherIconImageView.heightAnchor) ~ "weather square",
-        ])
-        
-        expandedDisplayModeConstraints = [
-            // image container full width at top
-            imageContainer.widthAnchor.constraintEqualToAnchor(widgetLayoutGuide.widthAnchor),
-            imageContainer.centerXAnchor.constraintEqualToAnchor(widgetLayoutGuide.centerXAnchor),
-            imageContainer.topAnchor.constraintEqualToAnchor(widgetLayoutGuide.topAnchor),
-            
-            // info container below image
-            infoContainer.widthAnchor.constraintEqualToAnchor(widgetLayoutGuide.widthAnchor),
-            infoContainer.centerXAnchor.constraintEqualToAnchor(widgetLayoutGuide.centerXAnchor),
-            infoContainer.topAnchor.constraintEqualToAnchor(imageContainer.bottomAnchor),
-            infoContainer.bottomAnchor.constraintEqualToAnchor(widgetLayoutGuide.bottomAnchor),
-
-            // title, and weather in horizontal row
-            titleLabel.leadingAnchor.constraintEqualToAnchor(infoContainer.leadingAnchor),
-            titleLabel.bottomAnchor.constraintEqualToAnchor(infoContainer.bottomAnchor),
-            weatherContainer.leadingAnchor.constraintEqualToAnchor(titleLabel.trailingAnchor),
-            weatherContainer.topAnchor.constraintEqualToAnchor(infoContainer.topAnchor),
-            weatherContainer.trailingAnchor.constraintEqualToAnchor(infoContainer.trailingAnchor),
-        ]
-        
-        compactDisplayModeConstraints = [
-            // image container full height on left
-            imageContainer.heightAnchor.constraintEqualToAnchor(widgetLayoutGuide.heightAnchor),
-            imageContainer.centerYAnchor.constraintEqualToAnchor(widgetLayoutGuide.centerYAnchor),
-            imageContainer.leadingAnchor.constraintEqualToAnchor(widgetLayoutGuide.leadingAnchor),
-            
-            // info container centred in remaining space
-            infoContainer.centerYAnchor.constraintEqualToAnchor(widgetLayoutGuide.centerYAnchor),
-            infoContainer.leadingAnchor.constraintEqualToAnchor(imageContainer.trailingAnchor),
-            infoContainer.trailingAnchor.constraintEqualToAnchor(widgetLayoutGuide.trailingAnchor),
-            
-            // title label centred in info
-            titleLabel.centerXAnchor.constraintEqualToAnchor(infoContainer.centerXAnchor),
-            titleLabel.widthAnchor.constraintLessThanOrEqualToAnchor(infoContainer.widthAnchor),
-            
-            // weather below title
-            weatherContainer.centerXAnchor.constraintEqualToAnchor(infoContainer.centerXAnchor),
-            weatherContainer.topAnchor.constraintEqualToAnchor(titleLabel.bottomAnchor),
-        ]
-        
-        widgetWidthConstraint = widgetLayoutGuide.widthAnchor.constraintEqualToConstant(0)
-        widgetHeightConstraint = widgetLayoutGuide.heightAnchor.constraintEqualToConstant(0)
-        widgetWidthConstraint.active = true
-        widgetHeightConstraint.active = true
-        
-        if #available(iOSApplicationExtension 10.0, *) {
-            guard let extensionContext = extensionContext else {
-                return
-            }
-            
-            extensionContext.widgetLargestAvailableDisplayMode = .Expanded
-
-            switch extensionContext.widgetActiveDisplayMode {
-            case .Expanded:
-                NSLayoutConstraint.activateConstraints(expandedDisplayModeConstraints)
-            case .Compact:
-                NSLayoutConstraint.activateConstraints(compactDisplayModeConstraints)
-            }
-        } else {
-            // pre iOS-10 only has expanded display mode
-            NSLayoutConstraint.activateConstraints(expandedDisplayModeConstraints)
-        }
+    private func layoutForExpandedDisplayMode(maxSize maxSize: CGSize) {
+        mainStackView.axis = .Vertical
+        infoStackView.axis = .Horizontal
+        titleLabel.textAlignment = .Left
     }
     
-    private func transitionFromConstraints(fromConstraints: [NSLayoutConstraint], toConstraints: [NSLayoutConstraint]) {
-        NSLayoutConstraint.deactivateConstraints(fromConstraints)
-        NSLayoutConstraint.activateConstraints(toConstraints)
+    private func layoutForCompactDisplayMode(maxSize maxSize: CGSize) {
+        mainStackView.axis = .Horizontal
+        infoStackView.axis = .Vertical
+        titleLabel.textAlignment = .Center
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        NSLog("view \(view.frame)")
-        NSLog("layoutGuide \(widgetLayoutGuide.layoutFrame)")
-        NSLog("imageContainer \(imageContainer!.frame)")
-        NSLog("infoContainer \(infoContainer!.frame)")
-        NSLog("titleLabel \(titleLabel!.frame)")
-    }
-}
-
-infix operator ~ { associativity left precedence 150 }
-private func ~ (constraint: NSLayoutConstraint, name: String) -> NSLayoutConstraint {
-    constraint.identifier = name
-    return constraint
-}
-
-private func ~ (constraint: NSLayoutConstraint, priority: UILayoutPriority) -> NSLayoutConstraint {
-    constraint.priority = priority
-    return constraint
 }
